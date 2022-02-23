71ebd0801c60b8378f5eebf2963f97fb7085f3c0
Marcus Quintela <marcus.quintela@gmail.com>
2022-02-23 10:26:09 -0300

    first commit

---
+++ src/main/java/com/client/parkingcontrolclient/ParkingControlClientApplication.java
@@ -0,0 +1,13 @@
+package com.client.parkingcontrolclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParkingControlClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParkingControlClientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/client/parkingcontrolclient/ParkingControlClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.client.parkingcontrolclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParkingControlClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
