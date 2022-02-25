1311765194bf5ce85f36f3a414f6c20e79b9ea99
Marcus Quintela <marcus.quintela@gmail.com>
2022-02-25 16:36:57 -0300

    Add genered client

--- src/main/java/com/client/parkingcontrolclient/ParkingControlClientApplication.java
+++ src/main/java/com/client/parkingcontrolclient/ParkingControlClientApplication.java
@@ -1,13 +1,27 @@
 package com.client.parkingcontrolclient;
 
+import io.swagger.client.ApiException;
+import io.swagger.client.api.ParkingSpotControllerApi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.UUID;
 
 @SpringBootApplication
 public class ParkingControlClientApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ParkingControlClientApplication.class, args);
-	}
+    public static void main(String[] args) throws ApiException {
+        SpringApplication.run(ParkingControlClientApplication.class, args);
+
+        ParkingSpotControllerApi parkingSpotControllerApi = new ParkingSpotControllerApi();
+
+        UUID id = UUID.fromString("e4f4283c-9d26-4f2d-9770-9aa130d8cc6e");
+
+        Object parkingSpotByIdUsingGET = parkingSpotControllerApi.getParkingSpotByIdUsingGET(id);
+
+        System.out.println(parkingSpotByIdUsingGET);
+
+
+    }
 
 }
